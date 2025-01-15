615237ef177a7cd544e26e848d63cf899ec3536a
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-14 19:37:52 -0500

    calendar - with a lot of errors

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,6 @@
 import { Fugaz_One } from 'next/font/google'
 import React from 'react'
+import Calendar from './Calendar'
 const fugaz = Fugaz_One({subsets: ['latin'], weight: ['400']})
 
 export default function Dashboard() {
@@ -42,6 +43,7 @@ export default function Dashboard() {
                 )
             })}
         </div>
+        <Calendar/>
     </div>
   )
 }
